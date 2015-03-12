e513f46bad6566d39395334fda0f177052702e9e
AdamHale <adamhale@Adams-MacBook-Pro.local>
2015-03-12 14:02:51 +0000

    deleting test
    I have deleted the test fie that I made a minute ago to confirm that
    github was working correctly

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
     define("DB_SERVER", "localhost");
-    define("DB_USER", "sgray"); //username
-    define("DB_PASS", "RSGErGQ6bto=");//password
-    define("DB_NAME", "sgray"); // database name (same as username for dakar)
+    define("DB_USER", "username"); //username
+    define("DB_PASS", "password=");//password
+    define("DB_NAME", "datebasename"); // database name (same as username for dakar)
 
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
